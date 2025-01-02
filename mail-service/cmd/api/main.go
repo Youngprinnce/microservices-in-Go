23ff033fe6179be2120dd39283cd614acd093059
youngprinnce <ajiboyeadedotun16@gmail.com>
2025-01-03 00:33:10 +0100

    Add mail sending functionality with SMTP integration and HTML/plain text templates

--- mail-service/cmd/api/main.go
+++ mail-service/cmd/api/main.go
@@ -4,16 +4,36 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 )
 
 type Config struct {
+	Mailer Mail
+}
 
+func createMail() Mail {
+	port, _ := strconv.Atoi(os.Getenv("MAIL_PORT"))
+	m := Mail {
+		Domain: os.Getenv("MAIL_DOMAIN"),
+		Host: os.Getenv("MAIL_HOST"),
+		Port: port,
+		Username: os.Getenv("MAIL_USERNAME"),
+		Password: os.Getenv("MAIL_PASSWORD"),
+		Encryption: os.Getenv("MAIL_ENCRYPTION"),
+		FromAddress: os.Getenv("FROM_ADDRESS"),
+		FromName: os.Getenv("FROM_NAME"),
+	}
+
+	return m
 }
 
 const webPort  = "80"
 
 func main( ) {
-	app := Config{}
+	app := Config{
+		Mailer: createMail(),
+	}
 
 	log.Println("starting mail service on port", webPort)
 
