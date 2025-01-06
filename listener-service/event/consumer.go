f55703c2c68e972cee7181f448b8f1280bfc1f84
youngprinnce <ajiboyeadedotun16@gmail.com>
2025-01-06 20:20:05 +0100

    Add RabbitMQ consumer and event handling functionality

---
+++ listener-service/event/consumer.go
@@ -0,0 +1,119 @@
+package event
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+// Type for recieving event from a queue
+type Consumer struct {
+	conn      *amqp.Connection
+	queueName string
+}
+
+// Create an instance of the consumer
+func NewConsumer(conn *amqp.Connection) (Consumer, error) {
+	consumer := Consumer{
+		conn: conn,
+	}
+
+	err := consumer.setup()
+	if err != nil {
+		return Consumer{}, err
+	}
+
+	return consumer, nil
+}
+
+func (consumer *Consumer) setup() error {
+	channel, err := consumer.conn.Channel()
+	if err != nil {
+		return err
+	}
+
+	return declareExchange(channel)
+}
+
+// Type for pushing event to the queue
+type Payload struct {
+	Name string `json:"name"`
+	Data string `json:"data"`
+}
+
+// Listen to topics on the queue
+func (consumer *Consumer) Listen(topics []string) error {
+	ch, err := consumer.conn.Channel()
+	if err != nil {
+		return err
+	}
+	defer ch.Close()
+
+	q, err := declareRandomQueue(ch)
+	if err != nil {
+		return err
+	}
+
+	// Bind the channels to each topic
+	for _, s := range topics {
+		ch.QueueBind(
+			q.Name,
+			s,
+			"logs_topics",
+			false,
+			nil,
+		)
+
+		if err != nil {
+			return err
+		}
+	}
+
+	// look for messages
+	messages, err := ch.Consume(q.Name, "", true, false, false, false, nil)
+	if err != nil {
+		return err
+	}
+
+	// Consume everything from rabbitMq till exist
+	forever := make(chan bool)
+	go func() {
+		for d := range messages {
+			var payload Payload
+			_ = json.Unmarshal(d.Body, &payload)
+
+			go handlePayload(payload)
+		}
+	}()
+
+	fmt.Printf("Waiting for message [Exchange, Queue] [logs_topic, %s]\n", q.Name)
+	<-forever
+
+	return nil
+}
+
+func handlePayload(payload Payload) {
+	switch payload.Name {
+	case "log", "event":
+		// log whatever we get
+		err := logEvent(payload)
+		if err != nil {
+			log.Println(err)
+		}
+
+	case "auth":
+		// authenticate
+
+	default:
+		err := logEvent(payload)
+		if err != nil {
+			log.Println(err)
+		}
+	}
+}
+
+func logEvent(entry Payload) error {
+	return nil
+}
---
+++ listener-service/event/event.go
@@ -0,0 +1,28 @@
+package event
+
+import (
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func declareExchange(ch *amqp.Channel) error {
+	return ch.ExchangeDeclare(
+		"logs_topic", // name of the excahnge
+		"topic",      // type
+		true,         // durable?
+		false,        // auto-deleted?
+		false,        // internal useage ?
+		false,        // no wait?
+		nil,          // arguments?
+	)
+}
+
+func declareRandomQueue(ch *amqp.Channel) (amqp.Queue, error) {
+	return ch.QueueDeclare(
+		"",    // name
+		false, // durable
+		false, // delete when unused?
+		true,  // esclusive?
+		false, // no wait?
+		nil,   // arguments
+	)
+}
